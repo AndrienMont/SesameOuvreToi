2305758a52cc8f3d7aa8e514d77b8d77fb81bc61
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2024-03-21 08:52:03 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>SesameOuvreToi</h1>
+<p>Entrée sur le campus du Bourget du lac</p>
